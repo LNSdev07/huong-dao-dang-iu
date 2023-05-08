fbc49fc7808003d9cc0d1f2bf74cecdf401ed675
Lại Ngọc Sơn <107608752+LNSdev07@users.noreply.github.com>
2023-05-08 14:31:52 +0700

    commit code
    commit code

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Hương Đào';
+const giftUrl = '';
+const eventName = 'I love you<3';
+const titleCard = 'Bé iu của anh <3';
+const contentCard = 'Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
